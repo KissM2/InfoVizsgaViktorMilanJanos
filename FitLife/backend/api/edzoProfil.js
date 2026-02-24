6722b06f8fd4e9a6a02387fa446803fc973a5c1f
BakonyiViktor <bakonyi.viktor2006@gmail.com>
2026-02-24 12:09:03 +0100

    edző profil végpont
    elkészítettem az edző profil végpontot ami egy id-t vár paraméterben és visszaadja egy bizonyos edző adatait.

--- FitLife/backend/api/edzoProfil.js
+++ FitLife/backend/api/edzoProfil.js
@@ -18,12 +18,35 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+//?GET /api/osszesEdzo
 router.get('/osszesEdzo', async (request, response) => {
     try {
         const edzok = await database.selectAllTrainers();
         response.status(200).json({
             message: 'Sikeres lekérdezés.',
             results: edzok
+        });
+    } catch (error) {
+        response.status(500).json({
+            message: 'Hiba történt a szerver oldalon.'
+        });
+    }
+});
+
+
+//?GET /api/edzoProfil?id=:id
+router.get('/edzoProfil?id=:id', async (request, response) => {
+    try {
+        if (!request.params.id) {
+            return response.status(400).json({
+                message: 'Hiányzó edző ID.'
+            });
+        }
+        request.params.id = request.params.id;
+        const edzo = await database.selectTrainerById(request.params.id);
+        response.status(200).json({
+            message: 'Sikeres lekérdezés.',
+            results: edzo
         });
     } catch (error) {
         response.status(500).json({
