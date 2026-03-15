830b91d123379ab4a64045ca5aee9be771016221
BakonyiViktor <bakonyi.viktor2006@gmail.com>
2026-03-15 08:31:58 +0100

    login, register adatbázis lekérdezések
    elkészítettem az adatbázis lekérdezéseket az új felhasználó és a belépés kezeléséhez.

--- FitLife/backend/sql/database.js
+++ FitLife/backend/sql/database.js
@@ -16,6 +16,20 @@ async function selectall() {
     const [rows] = await pool.execute(query);
     return rows;
 }
+
+async function insertUser(felh_nev, jelszo, email, telszam, nem, role, szul_datum){
+    const query = 'INSERT INTO login(felh_nev, jelszo, email, telszam, nem, role, szul_datum) values (?, ?, ?, ?, ?, ?, ?)';
+    const [rows] = await pool.execute(query, [felh_nev, jelszo, email, telszam, nem, role, szul_datum]);
+    return rows;
+}
+
+async function login(email) {
+    const query = 'SELECT login.jelszo FROM exampletable where login.email = ?;';
+    const [rows] = await pool.execute(query, [email]);
+    return rows;
+}
+
+
 //!Export
 module.exports = {
     selectall
