468c88bc8748be4fd51f78e86dca7142023f354e
KissM2 <119933843+KissM2@users.noreply.github.com>
2026-03-15 14:56:51 +0100

    felhasználói adatok frissítése és végpont javítása
    updateUser adatbázis lekérdezés létrehozása.
    /userData API végpont javítása

--- FitLife/backend/sql/database.js
+++ FitLife/backend/sql/database.js
@@ -16,7 +16,12 @@ async function selectall() {
     const [rows] = await pool.execute(query);
     return rows;
 }
+async function updateUser(cel_testsuly, cel_alak, uzott_sport, magassag, testsuly, edzesen_kivuli_mozgas, email) {
+    const query = "UPDATE felhasznalo INNER JOIN login ON felhasznalo.felhasznalo_id = login.id SET felhasznalo.cel_testsuly = ?, felhasznalo.cel_alak = ?, felhasznalo.uzott_sport = ?, felhasznalo.magassag = ?, felhasznalo.testsuly = ?, felhasznalo.edzesen_kivuli_mozgas = ? WHERE login.email = ?";
+    const [rows] = await pool.execute(query, [cel_testsuly, cel_alak, uzott_sport, magassag, testsuly, edzesen_kivuli_mozgas, email]);
+    return rows;
+}
 //!Export
 module.exports = {
-    selectall
+    selectall,updateUser
 };
