d1351844fcfb6d9b5ce20b840c13599db2eddcbf
KissM2 <119933843+KissM2@users.noreply.github.com>
2026-03-21 16:04:19 +0100

    bejelentkezes.js hiba javítása

--- FitLife/frontend/js/bejelentkezes.js
+++ FitLife/frontend/js/bejelentkezes.js
@@ -2,5 +2,5 @@ import { postKeres } from '../js/kozosFetch';
 document.getElementById('loginForm').addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    postKeres('/api/login', formData, '../html/index.html');
+    postKeres('/api/login', formData);
 });
