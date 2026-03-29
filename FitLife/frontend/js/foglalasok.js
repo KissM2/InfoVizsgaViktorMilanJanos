301744da4c214cb1bc4e60eeded1b4cc5007d512
Hyrtix68 <sipoczbendeguz@gmail.com>
2026-03-29 13:42:51 +0200

    ujfoglalas
    kivett időpontok tábla

--- FitLife/frontend/js/foglalasok.js
+++ FitLife/frontend/js/foglalasok.js
@@ -5,14 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
 const napok = ["Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"];
 
 const honapok = [
-"Január","Február","Március","Április","Május","Június",
-"Július","Augusztus","Szeptember","Október","November","December"
+    "Január", "Február", "Március", "Április", "Május", "Június",
+    "Július", "Augusztus", "Szeptember", "Október", "November", "December"
 ];
 
 let aktualisEv = new Date().getFullYear();
-let aktualisHonap = new Date().getMonth()+1;
+let aktualisHonap = new Date().getMonth() + 1;
 let kapottbeo = [];
-let mentettbeo = [[],[],[],[],[],[],[]];
+let mentettbeo = [[], [], [], [], [], [], []];
+let mentetttorolt=[];
 async function genBeo() {
     const host = document.getElementById('beo');
     for (let i = 0; i < napok.length; i++) {
@@ -35,7 +36,7 @@ async function genBeo() {
             date.dataset.di = i;
             date.addEventListener("click", function () {
                 let ez = this;
-                if (ez.dataset.bek==0) {
+                if (ez.dataset.bek == 0) {
                     ez.classList.add("bb");
                     ez.dataset.bek = 1;
                     mentettbeo[ez.dataset.di].push(ez.innerText);
@@ -56,27 +57,29 @@ async function genBeo() {
         host.appendChild(divtablecolumn);
     }
 }
-function naptarGeneral(ev,honap){
+function naptarGeneral(ev, honap) {
 
-    let hoEleje=new Date(ev,honap-1,1);
-    let hetnapja=hoEleje.getDay();
-    if(hetnapja==0) hetnapja=7;
+    let hoEleje = new Date(ev, honap - 1, 1);
+    let hetnapja = hoEleje.getDay();
+    if (hetnapja == 0) {
+        hetnapja = 7
+    };
 
-    let napszam=new Date(ev,honap,0).getDate();
-    let hetek = Math.ceil((hetnapja-1+napszam)/7);
+    let napszam = new Date(ev, honap, 0).getDate();
+    let hetek = Math.ceil((hetnapja - 1 + napszam) / 7);
 
-    let naptarinap=1;
+    let naptarinap = 1;
 
-    let naptar=document.createElement("div");
+    let naptar = document.createElement("div");
 
     // napok fejléc
-    let napSor=document.createElement("div");
+    let napSor = document.createElement("div");
     napSor.classList.add("het");
 
-    for(let n of napok){
-        let d=document.createElement("div");
-        d.classList.add("nap","fejlec");
-        d.innerText=n;
+    for (let n of napok) {
+        let d = document.createElement("div");
+        d.classList.add("nap", "fejlec");
+        d.innerText = n;
         napSor.appendChild(d);
     }
 
@@ -85,19 +88,52 @@ function naptarGeneral(ev,honap){
     // hetek
     for (let i = 0; i < hetek; i++) {
 
-        let het=document.createElement("div");
+        let het = document.createElement("div");
         het.classList.add("het");
 
         for (let j = 0; j < 7; j++) {
 
-            let nap=document.createElement("div");
+            let nap = document.createElement("div");
             nap.classList.add("nap");
 
-            if(i*7+j+1>=hetnapja && naptarinap<=napszam){
-                nap.innerText=naptarinap;
+            if (i * 7 + j + 1 >= hetnapja && naptarinap <= napszam) {
+                nap.innerText = naptarinap;
+                nap.addEventListener("click", function () {
+                    let ezz = this;
+
+                    for (let perc = 0; perc < 24 * 60; perc += 30) {
+                        let ora = Math.floor(perc / 60);
+                        let p = perc % 60;
+
+                        let ido = `${String(ora).padStart(2, "0")}:${String(p).padStart(2, "0")}`;
+                        let napgomb = document.createElement('button')
+                        napgomb.innerText = ido;
+                        napgomb.dataset.bek=0;
+                        if (mentetttorolt.includes(ido)) {
+                            napgomb.dataset.bek=1;
+                            napgomb.classList.add("rr");
+                        }
+                        napgomb.addEventListener("click", function () {
+                            let ez = this;
+                            if (ez.dataset.bek == 0) {
+                                ez.classList.add("rr");
+                                ez.dataset.bek = 1;
+                                mentetttorolt.push(ez.innerText);
+                            }
+                            else {
+                                if (mentetttorolt.length > 0) {
+                                    ez.classList.remove("rr");
+                                    ez.dataset.bek = 0;
+                                    mentetttorolt = mentetttorolt.filter(elem => elem !== ez.innerText);
+                                }
+                            }
+                            console.log(mentetttorolt)
+                        });
+                    }
+                });
                 naptarinap++;
             }
-            else{
+            else {
                 nap.classList.add("noDay");
             }
 
@@ -110,23 +146,23 @@ function naptarGeneral(ev,honap){
     return naptar;
 }
 
-function general(){
+function general() {
 
-    let kontener=document.getElementById("naptar");
-    kontener.innerHTML="";
+    let kontener = document.getElementById("naptar");
+    kontener.innerHTML = "";
 
     // fejléc
-    let fejlec=document.createElement("div");
+    let fejlec = document.createElement("div");
     fejlec.classList.add("naptarFejlec");
 
-    let bal=document.createElement("button");
-    bal.innerText="<";
+    let bal = document.createElement("button");
+    bal.innerText = "<";
 
-    let cim=document.createElement("span");
-    cim.innerText=aktualisEv+" "+honapok[aktualisHonap-1];
+    let cim = document.createElement("span");
+    cim.innerText = aktualisEv + " " + honapok[aktualisHonap - 1];
 
-    let jobb=document.createElement("button");
-    jobb.innerText=">";
+    let jobb = document.createElement("button");
+    jobb.innerText = ">";
 
     fejlec.appendChild(bal);
     fejlec.appendChild(cim);
@@ -134,27 +170,27 @@ function general(){
 
     kontener.appendChild(fejlec);
 
-    kontener.appendChild(naptarGeneral(aktualisEv,aktualisHonap));
+    kontener.appendChild(naptarGeneral(aktualisEv, aktualisHonap));
 
     // események
-    bal.addEventListener("click",function(){
+    bal.addEventListener("click", function () {
 
         aktualisHonap--;
 
-        if(aktualisHonap<1){
-            aktualisHonap=12;
+        if (aktualisHonap < 1) {
+            aktualisHonap = 12;
             aktualisEv--;
         }
 
         general();
     });
 
-    jobb.addEventListener("click",function(){
+    jobb.addEventListener("click", function () {
 
         aktualisHonap++;
 
-        if(aktualisHonap>12){
-            aktualisHonap=1;
+        if (aktualisHonap > 12) {
+            aktualisHonap = 1;
             aktualisEv++;
         }
 
