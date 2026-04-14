fb22d3f023e9173cdf29211fde8bb273d890670b
BakonyiViktor <bakonyi.viktor2006@gmail.com>
2026-04-14 09:12:37 +0200

    navbar profil gomb és telefonos nézet
    Elkészítettem a profil gombot a navbar-ra, ami csak akkor látható ha a felhasználó be van jelentkezve és a bejelentkezés és regisztrálás gombot váltja. A telefonos nézetben a profil a regisztráció és a bejelentkezés gombok csak a legördülő menüben jelennek meg.

--- FitLife/frontend/js/navbar.js
+++ FitLife/frontend/js/navbar.js
@@ -1,4 +1,6 @@
-export function navbarGeneralas(linkek) {
+import {getKeres} from '../js/kozosFetch.js';
+
+export async function navbarGeneralas(linkek) {
     const navbarBefogo = document.createElement('div');
     navbarBefogo.id = "navbar";
 
@@ -46,6 +48,17 @@ export function navbarGeneralas(linkek) {
     regisztralasBtn.innerText = "Regisztrálás";
     regisztralas.appendChild(regisztralasBtn);
 
+    const profilLink = document.createElement('a');
+    profilLink.classList.add("d-none", "d-sm-inline", "ms-2");
+    profilLink.style.textDecoration = "none";
+    profilLink.style.display = "none";
+
+    const profilBtn = document.createElement('button');
+    profilBtn.classList.add("gomb", "btn", "btn-secondary", "me-2");
+    profilBtn.innerText = "Profil";
+    profilBtn.style.display = "none";
+    profilLink.appendChild(profilBtn);
+
     const menuBtn = document.createElement('button');
     menuBtn.type = "button";
     menuBtn.classList.add("navbar-toggler", "border-0", "ms-2", "bg-hover-transp");
@@ -58,6 +71,7 @@ export function navbarGeneralas(linkek) {
 
     auth.appendChild(bejelentkezes);
     auth.appendChild(regisztralas);
+    auth.appendChild(profilLink)
     auth.appendChild(menuBtn);
 
     navbar.appendChild(auth);
@@ -79,6 +93,74 @@ export function navbarGeneralas(linkek) {
 
         listaElem.appendChild(a);
         menuLista.appendChild(listaElem);
+    }
+
+    const vonalElem = document.createElement('li');
+    vonalElem.classList.add("d-sm-none");
+
+    const vonal = document.createElement('hr');
+    vonal.classList.add("text-white", "mx-5");
+    vonalElem.appendChild(vonal);
+
+    const bejelentkezesTeloElem = document.createElement('li');
+    bejelentkezesTeloElem.classList.add("d-sm-none");
+
+    const bejelentkezesTeloLink = document.createElement('a');
+    bejelentkezesTeloLink.href = "/bejelentkezes";
+    bejelentkezesTeloLink.classList.add("nav-link", "text-white", "py-2", "font-size-medium")
+    bejelentkezesTeloLink.innerText = "Bejelentkezés";
+    bejelentkezesTeloElem.appendChild(bejelentkezesTeloLink);
+
+    const regisztralasTeloElem = document.createElement('li');
+    regisztralasTeloElem.classList.add("d-sm-none");
+
+    const regisztralasTeloLink = document.createElement('a');
+    regisztralasTeloLink.href = "/user_regisztral";
+    regisztralasTeloLink.classList.add("nav-link", "text-white", "py-2", "font-size-medium")
+    regisztralasTeloLink.innerText = "Regisztrálás";
+    regisztralasTeloElem.appendChild(regisztralasTeloLink);
+    
+    const profilTeloElem = document.createElement('li');
+    profilTeloElem.classList.add("d-sm-none");
+    profilTeloElem.style.display = "none";
+
+    const profilTeloLink = document.createElement('a');
+    profilTeloLink.classList.add("nav-link", "text-white", "py-2", "font-size-medium")
+    profilTeloLink.innerText = "Profil";
+    profilTeloElem.appendChild(profilTeloLink);
+
+    menuLista.appendChild(vonalElem);
+    menuLista.appendChild(bejelentkezesTeloElem);
+    menuLista.appendChild(regisztralasTeloElem);
+    menuLista.appendChild(profilTeloElem);
+
+    //még nincs meg az api végpont
+    //const bejelentkezve = await getKeres('/api/bejelentkezve');
+    const bejelentkezve = {
+        role: "teszt",
+        id: "teszt"
+    };
+    if(bejelentkezve && bejelentkezve.role && bejelentkezve.id){
+        bejelentkezesBtn.style.display = "none";
+        regisztralasBtn.style.display = "none";
+        bejelentkezesTeloElem.style.display = "none";
+        regisztralasTeloElem.style.display = "none";
+        profilBtn.style.display = "block";
+        profilLink.style.display = "block";
+        profilTeloElem.style.display = "block";
+        if(bejelentkezve.role == "felhasználó"){    
+            //még nincs profil oldala a felhasználónak és az edzőnek
+
+            //profilTeloLink.href = "/user_profil?id=" + bejelentkezve.id;
+            //profilLink.href = "/user_profil?id=" + bejelentkezve.id;
+        }else{
+            if(bejelentkezve.role == "edző"){
+                //még nincs profil oldala a felhasználónak és az edzőnek
+
+                //profilTeloLink.href = "/edzo_profil?id=" + bejelentkezve.id;
+                //profilLink.href = "/edzo_profil?id=" + bejelentkezve.id;
+            }
+        }
     }
 
     menuPontok.appendChild(menuLista);
