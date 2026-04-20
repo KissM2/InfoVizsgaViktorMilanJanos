2d7bf470140a859131b7d2799ce742c3d0c96282
BakonyiViktor <bakonyi.viktor2006@gmail.com>
2026-04-20 12:59:32 +0200

    user és edző survey stílus változtatása, selectek generálása és feltöltése
    Megváltoztattam a user és az edző survey stílusát a user és az edző profilhoz hasonlóra. Elhelyeztem két selectet a user surveybe a cél alak és az edzésen kívüli mozgás megadására, és ezeket feltöltöttem elemekkel a db-ből.

--- FitLife/frontend/js/userSurvey.js
+++ FitLife/frontend/js/userSurvey.js
@@ -9,6 +9,8 @@ document.addEventListener('DOMContentLoaded', async function(){
     valasztoGeneralasa('etelAllergiak', allergiak, etelAllergiakValasztott);
     valasztoGeneralasa('etelPrefferenciak', preferenciak, etelPrefferenciakValasztott);
 
+    selectFeltoltes();
+
     document.getElementById('submit').addEventListener('click', async function(e){
         e.preventDefault();
         const formData = new FormData(document.getElementById('surveyForm'));
@@ -25,7 +27,7 @@ function valasztoGeneralasa(id, lista, valasztott) {
     let befogo = document.getElementById(id);
     for (let i = 0; i < lista.length; i++) {
         let div = document.createElement('div');
-        div.classList.add('survey-item');
+        div.classList.add('survey-item', 'form-control', 'dark-input');
         div.innerText = lista[i].nev;
         befogo.appendChild(div);
         div.addEventListener('click', function(){
@@ -38,5 +40,27 @@ function valasztoGeneralasa(id, lista, valasztott) {
                 div.classList.add('selected');
             }
         });
+    }
+}
+
+async function selectFeltoltes() {
+    const celAlakOptions = (await getKeres('/api/getCelAlakOptions')).result;
+    const EKMOptions = (await getKeres('/api/getEKMOptions')).result;
+
+    const cel_alak_select = document.getElementById('cel_alak');
+    const EKMselect = document.getElementById('edzesen_kivuli_mozgas');
+
+    for (let i = 0; i < celAlakOptions.length; i++) {
+        let celAlakOptionElement = document.createElement('option');
+        celAlakOptionElement.innerText = celAlakOptions[i].nev;
+        celAlakOptionElement.value = celAlakOptions[i].id;
+        cel_alak_select.appendChild(celAlakOptionElement);
+    }
+    
+    for (let i = 0; i < EKMOptions.length; i++) {
+        let EKMOptionElement = document.createElement('option');
+        EKMOptionElement.innerText = EKMOptions[i].intenzitas;
+        EKMOptionElement.value = EKMOptions[i].id;
+        EKMselect.appendChild(EKMOptionElement);
     }
 }
